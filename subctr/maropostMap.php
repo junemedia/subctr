968736468da04d43ae74944d6b01a65292917622
John Shearer <john@ultranaut.com>
2016-03-10 10:27:08 -0500

    Reorder items in maropostMap
    Makes it easier for me to find items

--- subctr/maropostMap.php
+++ subctr/maropostMap.php
@@ -1,11 +1,45 @@
 <?php
 
 $maropostMap = array(
-  // The Feed by SavvyFork
-  583 => array(
-    'id' => 38261,
-    'name' => 'Savvy Fork The Feed'
+  //
+  0 => array(
+    'id' => 38507,
+    'name' => 'BR Legacy Import'
   ),
+
+  // Daily Recipes
+  '393' => array(
+    'id' => 38344,
+    'name' => 'R4L Daily Recipes'
+  ),
+  // Quick & Easy Recipes
+  394 => array(
+    'id' => 38352,
+    'name' => 'Quick And Easy Eats'
+  ),
+  // Recipe4Living Special Offers
+  396 => array(
+    'id' => 38345,
+    'name' => 'R4L Special Offers'
+  ),
+
+  // Fit&Fab Living Special Offers
+  410 => array(
+    'id' => 38364,
+    'name' => 'FF Special Offers'
+  ),
+  // Daily Insider
+  411 => array(
+    'id' => 38353,
+    'name' => 'FF Daily Insider'
+  ),
+  // Diet Insider
+  448 => array(
+    'id' => 38355,
+    'name' => 'FF Diet Insider'
+  ),
+
+
   // BetterRecipesDaily
   504 => array(
     'id' => 38298,
@@ -21,74 +55,44 @@ $maropostMap = array(
     'id' => 38338,
     'name' => 'Better Recipes Sweeps'
   ),
-  // IsSavvyforkSOLO
-  508 => array(
-    'id' => 38340,
-    'name' => 'SavvyFork Special Offers'
-  ),
   // IsRecipe4LivingSweeps
   507 => array(
     'id' => 38341,
     'name' => 'Recipe4Living Sweeps'
   ),
-  // Making It Work
-  553 => array(
-    'id' => 38342,
-    'name' => 'WIM '
-  ),
-  // Work It, Mom! Special Offers
-  558 => array(
-    'id' => 38343,
-    'name' => 'WIM Special Offers'
-  ),
-  // Daily Recipes
-  '393' => array(
-    'id' => 38344,
-    'name' => 'R4L Daily Recipes'
-  ),
-  // Recipe4Living Special Offers
-  396 => array(
-    'id' => 38345,
-    'name' => 'R4L Special Offers'
-  ),
-  // Casserole Cookin
-  539 => array(
-    'id' => 38346,
-    'name' => 'One Pot Wonders'
+  // IsSavvyforkSOLO
+  508 => array(
+    'id' => 38340,
+    'name' => 'SavvyFork Special Offers'
   ),
   // Crockpot Creations
   511 => array(
     'id' => 38346,
     'name' => 'One Pot Wonders'
   ),
-  // Quick & Easy Recipes
-  394 => array(
-    'id' => 38352,
-    'name' => 'Quick And Easy Eats'
+  // Casserole Cookin
+  539 => array(
+    'id' => 38346,
+    'name' => 'One Pot Wonders'
   ),
-  // Daily Insider
-  411 => array(
-    'id' => 38353,
-    'name' => 'FF Daily Insider'
-  ),
-  // Diet Insider
-  448 => array(
-    'id' => 38355,
-    'name' => 'FF Diet Insider'
+  // Making It Work
+  553 => array(
+    'id' => 38342,
+    'name' => 'WIM '
   ),
   // Copycat Classics
   554 => array(
     'id' => 38356,
     'name' => 'Copycat Classics'
   ),
-  // Fit&Fab Living Special Offers
-  410 => array(
-    'id' => 38364,
-    'name' => 'FF Special Offers'
+  // Work It, Mom! Special Offers
+  558 => array(
+    'id' => 38343,
+    'name' => 'WIM Special Offers'
   ),
-  //
-  0 => array(
-    'id' => 38507,
-    'name' => 'BR Legacy Import'
+  // The Feed by SavvyFork
+  583 => array(
+    'id' => 38261,
+    'name' => 'Savvy Fork The Feed'
   )
 );
