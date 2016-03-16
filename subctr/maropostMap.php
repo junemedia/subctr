9282490044d388a2aa0f4c3f711ac7528dc4b242
John Shearer <john@ultranaut.com>
2016-03-16 09:52:39 -0400

    Update new Maropost list mappings

--- subctr/maropostMap.php
+++ subctr/maropostMap.php
@@ -1,113 +1,107 @@
 <?php
 
 $maropostMap = array(
-  //
-  0 => array(
-    'id' => 38507,
-    'name' => 'BR Legacy Import'
-  ),
-
   // Daily Recipes
-  '393' => array(
-    'id' => 38344,
+  393 => array(
+    'id' => 41157,
     'name' => 'R4L Daily Recipes'
   ),
   // Quick & Easy Recipes
   394 => array(
-    'id' => 38352,
+    'id' => 41148,
     'name' => 'Quick And Easy Eats'
   ),
   // Budget Cooking (defunct)
   395 => array(
-    'id' => 38352,
+    'id' => 41148,
     'name' => 'Quick And Easy Eats'
   ),
   // Recipe4Living Special Offers
   396 => array(
-    'id' => 38345,
+    'id' => 41158,
     'name' => 'R4L Special Offers'
   ),
 
   // Fit&Fab Living Special Offers
   410 => array(
-    'id' => 38364,
+    'id' => 41106,
     'name' => 'FF Special Offers'
   ),
   // Daily Insider
   411 => array(
-    'id' => 38353,
+    'id' => 41102,
     'name' => 'FF Daily Insider'
   ),
   // Diet Insider
   448 => array(
-    'id' => 38355,
+    'id' => 41084,
     'name' => 'FF Diet Insider'
   ),
 
   // R4L Seasonal
   // Editors Choice (email link)
   502 => array(
-    'id' => 38344,
+    'id' => 41157,
     'name' => 'R4L Daily Recipes'
   ),
   // BetterRecipesDaily
   504 => array(
-    'id' => 38298,
+    'id' => 41136,
     'name' => 'Better Recipes Daily Recipes'
   ),
   // BetterRecipesSOLO
   505 => array(
-    'id' => 38333,
+    'id' => 41140,
     'name' => 'Better Recipes Special Offers'
   ),
   // IsBetterRecipesSweeps
   506 => array(
-    'id' => 38338,
+    'id' => 41154,
     'name' => 'Better Recipes Sweeps'
   ),
   // IsRecipe4LivingSweeps
   507 => array(
-    'id' => 38341,
+    'id' => 41141,
     'name' => 'Recipe4Living Sweeps'
   ),
   // IsSavvyforkSOLO
   508 => array(
-    'id' => 38340,
+    'id' => 41110,
     'name' => 'SavvyFork Special Offers'
   ),
   // Crockpot Creations
   511 => array(
-    'id' => 38346,
+    'id' => 41155,
     'name' => 'One Pot Wonders'
   ),
   // Casserole Cookin
   539 => array(
-    'id' => 38346,
+    'id' => 41155,
     'name' => 'One Pot Wonders'
   ),
   // Making It Work
   553 => array(
-    'id' => 38342,
+    'id' => 41111,
     'name' => 'WIM '
   ),
   // Copycat Classics
   554 => array(
-    'id' => 38356,
+    'id' => 41152,
     'name' => 'Copycat Classics'
   ),
   // Work It, Mom! Special Offers
   558 => array(
-    'id' => 38343,
+    'id' => 41113,
     'name' => 'WIM Special Offers'
   ),
   // Diabetic-Friendly Dishes (defunct)
-  '574' => array(
-    'id' => 38344,
+  574 => array(
+    'id' => 41157,
     'name' => 'R4L Daily Recipes'
   ),
   // The Feed by SavvyFork
   583 => array(
-    'id' => 38261,
+    'id' => 41109,
     'name' => 'Savvy Fork The Feed'
   )
 );
